6ef121c3d390c74b9d8695232070421666694b32
AfitovaViktoriya <afitovaviktoria@gmail.com>
2022-05-31 11:36:07 +0300

    added components and router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
+import Profile from "./components/Profile/Profile";
+import Dialogues from "./components/Dialogues/Dialogues";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <h1>social network</h1>
-    </div>
+    <BrowserRouter>
+      <div className="wrapper">
+        <Header />
+        <Navbar />
+        <div className="wrapper-content">
+          <Routes>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/dialogues" element={<Dialogues />} />
+
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
